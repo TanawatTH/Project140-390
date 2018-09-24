db6c9310e7e04433cf6e8e6de3ec09b20cc99037
unknown <Poramat Nukliang>
2018-09-24 10:46:51 +0700

    save

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require ('express');
 var pgp = require('pg-promise')();
 var db  =pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+
+
 
   //app.use(express.static('static'));
   app.set('view engine', 'ejs');
@@ -99,6 +106,27 @@ app.get('/users', function(req, res) {
   
       })
   });
+
+//update data
+app.post('/products/update',function(req, res){
+    var id = req.body.id;
+    var title = req.body.title;
+    var price = req.body.price;
+    res.send('id : '+ id + ',title = ' + title + ',price = ' + price);
+
+
+    
+});
+
+
+
+
+
+
+
+
+
+
 console.log('App is running at http://localhost:8080');
 
 app.listen(8080);
