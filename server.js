482265c31a2ff208c647947172d78a8b33a28d06
poramat nukliang <marnamfarday@gmail.com>
2018-10-20 18:13:36 +0700

    delete1

--- server.js
+++ server.js
@@ -139,15 +139,15 @@ app.get('/product_delete/:pid',function (req, res) {
     db.any(sql)
         .then(function(data){
             console.log('DATA:'+data);
-            res.render('pages/products',{products : data});
-            
+            res.redirect('/products');
+    
         })
         .catch(function(data){
                 console.log('ERROR:'+console.error);
                 
     })
  });
- 
+
  //add Product
 app.get('/insert',function (req, res) {
     res.render('pages/insert'); 
